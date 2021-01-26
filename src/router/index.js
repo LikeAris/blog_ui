dcf9f07f9d4a88cfbb96f9c5dbc7931175db4a8e
LikeAris <1815836466@qq.com>
2021-01-26 11:51:21 +0800

    2021年1月26日11:51:13

---
+++ src/router/index.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import router from 'vue-router';
+
+vue.use(router);
---
+++ src/vuex/store.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+vue.use(Vuex);
+
+export default new Vuex.store({
+    state:{},
+    mutations:{}
+})
