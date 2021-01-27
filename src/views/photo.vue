3ea0c7feeb81c1e36760024dd886f639fcd75564
LikeAris <1815836466@qq.com>
2021-01-27 14:37:42 +0800

    2021年1月27日14:37:06

---
+++ src/views/photo.vue
@@ -0,0 +1,31 @@
+<!--主页面-->
+<template>
+    <div>
+        <Carousel></Carousel>
+    </div>
+</template>
+
+<script>
+    import Carousel from "../components/carousel.vue";
+    // 导出模块
+    export default {
+        name:'Photo',
+        data() {
+            return {
+            };
+        },
+        watch: {
+        },
+        computed: {
+        },
+        methods: {
+        },
+        components:{
+            Carousel
+        }
+    }
+</script>
+<!-- scoped 样式只在本组件使用-->
+<style scoped>
+
+</style>
